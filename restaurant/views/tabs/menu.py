0d7b7f77d8961ea8c72e915bfddbfd44b777cb55
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-01-24 13:33:06 +0900

    Create menu.py

---
+++ restaurant/views/tabs/menu.py
@@ -0,0 +1,12 @@
+import flet as ft
+import datetime
+
+
+def menu(page: ft.Page):
+    return ft.View(
+        route='/user_mainscreen',
+        bgcolor='#000000',
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[ft.Text('menu', color='black', size=30)],
+    )

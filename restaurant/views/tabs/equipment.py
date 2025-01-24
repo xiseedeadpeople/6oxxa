b4d9e2e941f5c5769d053722b0246618ba79ff13
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-01-24 13:32:47 +0900

    Create equipment.py

---
+++ restaurant/views/tabs/equipment.py
@@ -0,0 +1,88 @@
+import flet as ft
+
+
+class ExpandableItem:
+    def __init__(self, name, options):
+        self.name = name
+        self.options = options
+
+    def create(self):
+        option_column = ft.Column(visible=False)
+
+        # options customization
+        for option in self.options:
+            row = ft.Row(
+                [
+                    ft.Text(option, color='white'),
+                    ft.ElevatedButton("Включить", on_click=lambda e, opt=option: self.enable_option(opt)),
+                ],
+                alignment=ft.MainAxisAlignment.END,
+                height=50
+            )
+            option_column.controls.append(row)
+
+        item_row = ft.Container(
+            content=ft.Row(
+                [
+                    ft.Text(self.name, color='white'),
+                    ft.IconButton(
+                        icon=ft.icons.ADD,
+                        on_click=lambda e: self.toggle_expansion(e, option_column),
+                        data=option_column,
+                    ),
+                ],
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                height=60,
+            ),
+            padding=10,
+            bgcolor='#202020',
+            width=350,
+            height=150,
+            border_radius=20,
+        )
+
+        # Устанавливаем обработчик клика на строку элемента
+        item_row.on_click = lambda e: self.toggle_expansion(e, option_column)
+
+        return ft.Column([item_row, option_column])
+
+    def toggle_expansion(self, e, option_column):
+        option_column.visible = not option_column.visible
+        e.page.update()
+
+    def enable_option(self, option):
+        print(f"Опция '{option}' включена!")
+
+
+def equipment(page: ft.Page):
+    page.title = "Расширяемые элементы с опциями"
+    page.window.width = 390
+    page.window.height = 844
+    page.window.always_on_top = True
+    page.window.maximizable = False
+    page.window.resizable = False
+    page.bgcolor = 'black'
+    page.padding = 20
+    page.spacing = 20
+
+    page.horizontal_alignment = 'center'
+
+    # Создание ListView с контролами
+    list_view = ft.ListView(
+        spacing=10,
+        controls=[
+            item.create() for item in [
+                ExpandableItem('Кухня', ['Умный холодильник', 'Умная плита', 'Системы управления запасами']),
+                ExpandableItem('Безопасность и контроль', ['Камеры', 'Температура холодильника'])
+            ]
+        ]
+    )
+
+    return ft.View(
+        route='/user_mainscreen',
+        bgcolor='#000000',
+        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[list_view],  # Тут мы вставляем наш ListView
+    )
+

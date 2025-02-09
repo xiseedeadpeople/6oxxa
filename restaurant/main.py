8228a8f325856ce7088e4bc662e0f758ecf8ccf1
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-09 22:19:08 +0900

    Update main.py

--- restaurant/main.py
+++ restaurant/main.py
@@ -4,13 +4,11 @@ from views.mainscreen import main_screen
 
 from views.tabs.reservation import reservation
 from views.tabs.ordershistory import orders_history
-from views.tabs.menu import menu
 from views.tabs.equipment import equipment
 from views.tabs.staff import staff
 
 
 def main(page: ft.Page):
-
     page.window.width = 390
     page.window.height = 844
     page.window.always_on_top = True
@@ -21,10 +19,11 @@ def main(page: ft.Page):
 
     def route_change(route):
         page.views.clear()
-        page.views.append(login_screen(page))
 
+        if page.route == '/':
+            page.views.append(login_screen(page))
 
-        if page.route == "/user_mainscreen":
+        elif page.route == "/user_mainscreen":
             page.views.append(main_screen(page))
 
         elif page.route == '/user_mainscreen/reservation':
@@ -33,27 +32,22 @@ def main(page: ft.Page):
         elif page.route == '/user_mainscreen/orders_history':
             page.views.append(orders_history(page))
 
-        elif page.route == '/user_mainscreen/menu':
-            page.views.append(menu(page))
-
         elif page.route == '/user_mainscreen/equipment':
             page.views.append(equipment(page))
 
         elif page.route == '/user_mainscreen/staff':
             page.views.append(staff(page))
-
-        # manager tabs
-
         page.update()
+
 
     def view_pop(view):
         page.views.pop()
         top_view = page.views[-1]
         page.go(top_view.route)
 
+
     page.on_route_change = route_change
     page.on_view_pop = view_pop
-
     page.go(page.route)
 
 

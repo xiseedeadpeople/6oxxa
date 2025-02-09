98819ba2b8d089c052fab1574b51e1b207f2696d
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-09 22:23:29 +0900

    Update loginscreen.py

--- restaurant/views/loginscreen.py
+++ restaurant/views/loginscreen.py
@@ -1,21 +1,10 @@
-from prefs import colors
+from prefs.colors import colors, style, error_style
+from prefs.fonts.font_setup import font_list
 import flet as ft
-import re
-
 
 
 def login_screen(page: ft.Page):
-    page.fonts = {
-        "SteppeBlack": "fonts/Steppe-Black.ttf",
-        "SteppeExtraBold": "fonts/Steppe-ExtraBold.ttf",
-        "SteppeBold": "fonts/Steppe-Bold.ttf",
-        "SteppeSemiBold": "fonts/Steppe-SemiBold.ttf",
-        "SteppeMedium": "fonts/Steppe-Medium.ttf",
-        "SteppeRegular": "fonts/Steppe-Regular.ttf",
-        "SteppeBook": "fonts/Steppe-Book.ttf",
-        "SteppeLight": "fonts/Steppe-Light.ttf",
-        "SteppeThin": "fonts/Steppe-Thin.ttf",
-    }
+    page.fonts = font_list
 
     def password_focus(e):
         password.error_text = ''
@@ -24,25 +13,15 @@ def login_screen(page: ft.Page):
     def password_blur(e):
         if len(password.value) == 0:
             password.error_text = 'Введите пароль'
-        elif 1 <= len(password.value) < 8:
-            password.error_text = 'Слишком короткий!'
-
-        elif not re.search(r'[0-9]', password.value):
-            password.error_text = 'В пароле должна быть цифра!'
-        elif not re.search(r'[A-Z]', password.value):
-            password.error_text = 'А заглавная буква?'
-        elif not re.search(r'[a-z]', password.value):
-            password.error_text = 'А маленькая буква?'
-        elif not re.search(r'[.@#$%/^&+=?!,]', password.value):
-            password.error_text = 'А спец. символ? (.@#$%/^&+=?!,)'
-
         page.update()
 
     def phone_focus(e):
+        phone.prefix_style = style(color='black')
         phone.error_text = ''
         page.update()
 
     def phone_blur(e):
+        phone.prefix_style = style()
         if len(phone.value) == 0:
             phone.error_text = 'Введите номер'
 
@@ -53,124 +32,98 @@ def login_screen(page: ft.Page):
     def format_phone(e):
         phone.error_text = ''
         new_value = (phone.value.replace(" ", "").replace("(", "")
-                     .replace(")", "").replace("-", ""))
+                     .replace(")", "").replace("-", ""))  # Убираем все ненужные символы
 
-        if len(new_value) > 14:
-            new_value = new_value[:14]
+        # # # Ограничиваем длину введенной строки
+        if len(phone.value) > 9:
+            phone.value = new_value[:11]
+            formatted_value = f'({phone.value[:3]}) {new_value[3:6]}-{new_value[6:8]}-{new_value[8:10]}'
+            phone.value = formatted_value
+            password.focus()
+            phone.update()
 
-        formatted_value = ""
-        if len(new_value) >= 1:
-            formatted_value += f"({new_value[:3]})"
-        if len(new_value) >= 4:
-            formatted_value += f" {new_value[3:6]}"
-        if len(new_value) >= 7:
-            formatted_value += f"-{new_value[6:8]}"
-        if len(new_value) >= 9:
-            formatted_value += f"-{new_value[8:11]}"
-
-        phone.value = formatted_value
-        page.update()
-
-    def on_button_click(e):
-        print(f'passforgot_btn triggered.')
+    dlg = ft.AlertDialog(barrier_color=ft.Colors.with_opacity(0.96, 'black'),
+                         title=ft.Text('password: 123', style=style(size=25)),
+                         bgcolor=ft.Colors.with_opacity(0, '#080808'))
 
     passforgot_btn = ft.CupertinoButton(
         content=ft.Container(ft.Text('Забыли пароль?',
-                                     style=ft.TextStyle(color=colors['inv_primarytext'], size=20,
-                                                        font_family='SteppeThin',
-                                                        shadow=ft.BoxShadow(color=colors['inv_fontshadow'],
-                                                        spread_radius=1000, offset=(0, 0), blur_radius=1,
-                                                        blur_style=ft.ShadowBlurStyle.OUTER)))),
-        on_click=lambda e: print(f'forgot pass trigg'))
+                                     style=style(color=colors['inv_primarytext'],
+                                                 shadowclr=colors['inv_fontshadow'], spreadshadow=100)),
+        on_click=lambda e: page.open(dlg)))
 
     phone = ft.TextField(
+        cursor_color = 'black',
+        focused_color='black',
         label='Номер телефона',
         width=300,
         border_radius=10,
         prefix_text='+7 ',
-        label_style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeThin', shadow=ft.BoxShadow(color='#307849')),
-        text_style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeRegular'),
-        prefix_style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeRegular'),
-        error_style=ft.TextStyle(color=colors['error'], size=15, font_family='SteppeSemiBold'),
+        label_style=style(),
+        text_style=style(),
+        prefix_style=style(color='black'),
+        error_style=error_style(size=15),
         adaptive=True,
-        border=ft.InputBorder.UNDERLINE,
+        border=ft.InputBorder.NONE,
         fill_color=colors['bg'],
-        input_filter=ft.InputFilter(allow=True, regex_string=r"^[0-9()\- ]*$", replacement_string=""),
+        input_filter=ft.NumbersOnlyInputFilter(),
         on_change=format_phone,
         on_blur=phone_blur, on_focus=phone_focus
     )
 
-    password = ft.TextField(label="Пароль", width=300, border_radius=10, password=True, can_reveal_password=True,
-                            label_style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeThin'),
-                            text_style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeRegular'),
-                            prefix_style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeRegular'),
-                            error_style=ft.TextStyle(color=colors['error'], size=15, font_family='SteppeSemiBold'),
-                            on_focus=password_focus, on_blur=password_blur,
-                            border=ft.InputBorder.UNDERLINE, fill_color=colors['bg'], adaptive=True,
-                            )
-
-    mybarx = ft.Container(
-        gradient=ft.LinearGradient(begin=ft.alignment.top_left,
-                                   end=ft.alignment.bottom_right,
-                                   colors=colors['gradient']),
-
-        border_radius=ft.border_radius.vertical(bottom=30),
-        shadow=ft.BoxShadow(spread_radius=3, blur_radius=50, color=colors['second_shadow']),
-        width=page.window.width, height=150, alignment=ft.alignment.center,
-        content=ft.Container(ft.Text('vxxc',
-                                     style=ft.TextStyle(color=colors['inv_primarytext'], size=100,
-                                                        font_family='SteppeThin',
-                                                        shadow=ft.BoxShadow(color=colors['inv_fontshadow'],
-                                                        spread_radius=1000,
-                                                        offset=(0, 0), blur_radius=1,
-                                                        blur_style=ft.ShadowBlurStyle.OUTER))))
+    password = ft.TextField(
+        cursor_color='black',
+        focused_color = 'black',
+        label='Пароль',
+        width=300,
+        border_radius=10,
+        password=True,
+        can_reveal_password=True,
+        label_style=style(),
+        text_style=style(),
+        prefix_style=style(),
+        error_style=error_style(size=15),
+        on_focus=password_focus, on_blur=password_blur,
+        border=ft.InputBorder.NONE, fill_color=colors['bg'], adaptive=True,
     )
 
-    default_users = {}
-    managers = {'1': '1'}
+    mybarx = ft.Container(
+        bgcolor=colors['bg'],
+        border_radius=ft.border_radius.vertical(bottom=30),
+        shadow=ft.BoxShadow(spread_radius=1, blur_radius=100, color=colors['block_shadow']),
+        width=page.window.width, height=150, alignment=ft.alignment.center,
+        content=ft.Container(ft.Text('tellmewhy✦₊ﾟ', style=style(color=colors['primary'],
+                                                            shadowclr=colors['font_shadow'], size=20)))
+    )
+
 
     def go_to_welcome(e):
 
-        # phone
-        # if len(phone.value) == 0:
-        #     phone.error_text = 'Введите номер'
-        # elif 1 <= len(phone.value) < 14:
-        #     phone.error_text = 'Неверный формат номера'
-        #
-        # # password
-        # if len(password.value) == 0:
-        #     password.error_text = 'Введите пароль'
-        # elif 1 <= len(password.value) < 8:
-        #     password.error_text = 'Слишком короткий пароль!'
-        # elif not re.search(r'[0-9]', password.value):
-        #     password.error_text = 'В пароле должна быть цифра!'
-        # elif not re.search(r'[A-Z]', password.value):
-        #     password.error_text = 'А заглавная буква?'
-        # elif not re.search(r'[a-z]', password.value):
-        #     password.error_text = 'А маленькая буква?'
-        # elif not re.search(r'[.@#$%/^&+=?!,]', password.value):
-        #     password.error_text = 'А спец. символ? (.@#$%/^&+=?!,)'
-        #
-        # else:
-        #     print(f'user: +7{phone.value.replace("(", "").replace(")", "").replace("-", "").replace(" ", "")}'),
-        #     print(f'password: {password.value}')
+        if len(phone.value) == 0:
+            phone.error_text = 'Введите номер'
+            phone.update()
+
+        elif len(phone.value) != 15:
+            phone.error_text = 'Неверный формат номера'
+            phone.update()
+
+        if len(password.value) == 0:
+            password.error_text = 'Введите пароль'
+            password.update()
+
+        elif password.value != '123':
+            password.error_text = 'Неверный пароль!'
+            password.update()
+
+        elif password.value == '123' and len(phone.value) == 15:
             page.go("/user_mainscreen")
 
     btn = ft.Container(
-        ft.Text('ВОЙТИ',
-                style=ft.TextStyle(color=colors['primary'], size=20, font_family='SteppeThin',
-                                   shadow=ft.BoxShadow(color=colors['font_shadow'],
-                                                       spread_radius=10,
-                                                       offset=(0, 0), blur_radius=4,
-                                                       blur_style=ft.ShadowBlurStyle.OUTER))),
-
+        ft.Text('Войти', style=style()),
         width=300, height=50, on_click=go_to_welcome, border_radius=20, alignment=ft.alignment.center,
-        shadow=ft.BoxShadow(spread_radius=3, blur_radius=50, color=colors['second_shadow']),
+        shadow=ft.BoxShadow(spread_radius=0.5, blur_radius=50, color=colors['block_shadow']),
+        bgcolor=colors['bg'])
 
-        gradient=ft.LinearGradient(
-            begin=ft.alignment.top_left,
-            end=ft.alignment.bottom_right,
-            colors=colors['gradient']))
 
     main_container = ft.Column(
         controls=[

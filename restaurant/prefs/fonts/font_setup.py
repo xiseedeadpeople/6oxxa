eecef04ab90d295dfa98fa1faa005fb62d60e3e8
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-09 22:22:33 +0900

    Update font_setup.py

--- restaurant/prefs/fonts/font_setup.py
+++ restaurant/prefs/fonts/font_setup.py
@@ -1,12 +1,12 @@
 font_list = {
-    "SteppeBlack": "prefs/fonts/Steppe-Black.ttf",
-    "SteppeExtraBold": "prefs/fonts/Steppe-ExtraBold.ttf",
-    "SteppeBold": "prefs/fonts/Steppe-Bold.ttf",
-    "SteppeSemiBold": "prefs/fonts/Steppe-SemiBold.ttf",
-    "SteppeMedium": "prefs/fonts/Steppe-Medium.ttf",
-    "SteppeRegular": "prefs/fonts/Steppe-Regular.ttf",
-    "SteppeBook": "prefs/fonts/Steppe-Book.ttf",
-    "SteppeLight": "prefs/fonts/Steppe-Light.ttf",
-    "SteppeThin": "prefs/fonts/Steppe-Thin.ttf",
+    # "SteppeBlack": "prefs/fonts/Steppe-Black.ttf",
+    # "SteppeExtraBold": "prefs/fonts/Steppe-ExtraBold.ttf",
+    # "SteppeBold": "prefs/fonts/Steppe-Bold.ttf",
+    # "SteppeSemiBold": "prefs/fonts/Steppe-SemiBold.ttf",
+    # "SteppeMedium": "prefs/fonts/Steppe-Medium.ttf",
+    # "SteppeRegular": "prefs/fonts/Steppe-Regular.ttf",
+    # "SteppeBook": "prefs/fonts/Steppe-Book.ttf",
+    # "SteppeLight": "prefs/fonts/Steppe-Light.ttf",
+    # "SteppeThin": "prefs/fonts/Steppe-Thin.ttf",
     "basic": "prefs/fonts/minecraft.ttf"
 }

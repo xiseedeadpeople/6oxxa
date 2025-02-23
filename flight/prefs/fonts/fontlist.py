c129792065ba12f43fba0c87cbbdf440e642ef6f
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-23 18:57:53 +0900

    Create fontlist.py

---
+++ flight/prefs/fonts/fontlist.py
@@ -0,0 +1,11 @@
+fonts = {
+    "regular": "prefs/fonts/SFPRODISPLAYREGULAR.otf",
+
+    "w": "prefs/fonts/Ghastly Panic.ttf",
+    "light italic": "prefs/fonts/SFPRODISPLAYLIGHTITALIC.otf",
+    "medium": "prefs/fonts/SFPRODISPLAYMEDIUM.otf",
+    "thin": "prefs/fonts/SFPRODISPLAYTHINITALIC.otf",
+    "semi bold": "prefs/fonts/SFPRODISPLAYSEMIBOLDITALIC.otf",
+    "bold": "prefs/fonts/SFPRODISPLAYBOLD.otf",
+    "black italic": "prefs/fonts/SFPRODISPLAYBLACKITALIC.otf"
+}

d147dff58d06135fa0202e49d283326bd30d331f
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-24 13:57:24 +0900

    Update orders_history.py

--- flight/views/tabs/orders_history.py
+++ flight/views/tabs/orders_history.py
@@ -1,12 +1,21 @@
 import flet as ft
-import datetime
-from prefs.colors import colors, style
+from datetime import datetime
+from flight.prefs.colors import colors, style
+from flight.models import Session, Flight
 
 def ordrs_history(page: ft.Page):
+    session = Session()
+    past_flights = session.query(Flight).filter(Flight.departure_date < datetime.now()).all()
+    session.close()
+
+    flight_texts = []
+    for flight in past_flights:
+        flight_texts.append(ft.Text(f"От: {flight.from_city}, Куда: {flight.to_city}, Дата: {flight.departure_date.strftime('%d/%m/%Y')}", style=style()))
+
     return ft.View(
         route='/user_mainscreen',
         bgcolor=colors['bg'],
         vertical_alignment=ft.MainAxisAlignment.CENTER,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        controls=[ft.Text('ordrs_history', style=style())],
+        controls=flight_texts or [ft.Text('Нет завершенных рейсов', style=style())],
     )

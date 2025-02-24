e75b07c819548e54087f54df0d1107b85b27c506
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-24 13:57:35 +0900

    Update closest_flights.py

--- flight/views/tabs/closest_flights.py
+++ flight/views/tabs/closest_flights.py
@@ -1,13 +1,21 @@
 import flet as ft
-import datetime
-from prefs.colors import colors, style
+from datetime import datetime
+from flight.prefs.colors import colors, style
+from flight.models import Session, Flight
 
 def closest_flights(page: ft.Page):
+    session = Session()
+    upcoming_flights = session.query(Flight).filter(Flight.departure_date >= datetime.now()).all()
+    session.close()
+
+    flight_texts = []
+    for flight in upcoming_flights:
+        flight_texts.append(ft.Text(f"От: {flight.from_city}, Куда: {flight.to_city}, Дата: {flight.departure_date.strftime('%d/%m/%Y')}", style=style()))
 
     return ft.View(
         route='/user_mainscreen',
         bgcolor=colors['bg'],
         vertical_alignment=ft.MainAxisAlignment.CENTER,
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        controls=[ft.Text('closest_flights', style=style())],
+        controls=flight_texts or [ft.Text('Нет ближайших рейсов', style=style())],
     )

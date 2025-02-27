bf200de3b7c3d6cb50715bd1881d6a94f937480b
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-27 15:31:17 +0900

    Update and rename colors.py to stylesnhelpers.py

--- flight/prefs/stylesnhelpers.py
+++ flight/prefs/stylesnhelpers.py
@@ -1,17 +1,13 @@
 import flet as ft
-colors = {
-    # 'primary': '#5117FF',
-    # 'secondary': '#1e3028',
-    # 'bg': '#111111',
-    # 'font_shadow': '#5117FF',
-    # 'gradient': ['#abff9e', '#000000'],
-    # 'second_shadow': '#abff9e',
-    # 'error': '#ff0000',
-    # 'error_shadow': '#7B0909',
-    # 'block_shadow': '#000000',
-    # 'inv_primarytext': '#000000',
-    # 'inv_fontshadow': '#AF94FF',
+import codecs
 
+fileObj = codecs.open( "prefs/txt-cities-russia.txt", "r", "utf_8_sig" )
+text = fileObj.readlines()
+fileObj.close()
+cities = list(map(str.strip, text))
+
+
+colors = {
     'primary': '#000000',
     'secondary': '#1e3028',
     'bg': '#FFFFFF',
@@ -25,7 +21,8 @@ colors = {
     'inv_fontshadow': '#AF94FF',
 }
 
-def style(color=None, size=25, font_family='regular', shadowclr=None, spreadshadow=10):
+
+def style(color=None, size=25, font_family='primary', shadowclr=None, spreadshadow=10):
     if color is None:
         color = colors['primary']
     if shadowclr is None:
@@ -36,7 +33,7 @@ def style(color=None, size=25, font_family='regular', shadowclr=None, spreadshad
                                             blur_style=ft.ShadowBlurStyle.OUTER))
 
 
-def error_style(color=None, size=25, font_family='regular', shadowclr=None, spreadshadow=10):
+def error_style(color=None, size=25, font_family='primary', shadowclr=None, spreadshadow=10):
     if color is None:
         color = colors['error']
     if shadowclr is None:
@@ -45,3 +42,30 @@ def error_style(color=None, size=25, font_family='regular', shadowclr=None, spre
                         shadow=ft.BoxShadow(color=shadowclr, spread_radius=spreadshadow,
                                             offset=(0, 0), blur_radius=4,
                                             blur_style=ft.ShadowBlurStyle.OUTER))
+
+def show_snackbar(page, message, textsize=15, textcolor='white', ff='regular', bgcolor='black'):
+    snackbar = ft.SnackBar(
+        ft.Container(ft.Text(message, style=style(size=textsize, color=textcolor, shadowclr=bgcolor,
+                                                  font_family=ff)), alignment=ft.alignment.center),
+        open=True, bgcolor=bgcolor, duration=1000
+    )
+    page.overlay.append(snackbar)
+    page.update()
+
+def tfield(label, ):
+    return ft.TextField(
+        cursor_color='black',
+        focused_color='black',
+        label=f'{label}',
+        width=300,
+        border_radius=10,
+        label_style=style(),
+        text_style=style(),
+        prefix_style=style(color='black'),
+        error_style=error_style(size=10, shadowclr=colors['bg']),
+        adaptive=True,
+        border=ft.InputBorder.NONE,
+        fill_color=colors['bg'],
+    )
+
+

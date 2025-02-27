9cf713ad5e29bde136e4c9bec086ed9e53874e79
iseedeadpeople <149659358+xiseedeadpeople@users.noreply.github.com>
2025-02-27 15:26:58 +0900

    Update models.py

--- flight/models.py
+++ flight/models.py
@@ -14,6 +14,18 @@ class Flight(Base):
     departure_date = Column(Date)
     return_date = Column(Date)
 
+class PurchaseHistory(Base):
+    __tablename__ = 'purchase_history'
+
+    id = Column(Integer, primary_key=True)
+    from_city = Column(String)
+    to_city = Column(String)
+    departure_date = Column(Date)
+    return_date = Column(Date)
+
+    def __repr__(self):
+        return f"<PurchaseHistory(from_city='{self.from_city}', to_city='{self.to_city}', " \
+               f"departure_date='{self.departure_date}', return_date='{self.return_date}')>"
 
 engine = create_engine('sqlite:///flights.db')
 Base.metadata.create_all(engine)
